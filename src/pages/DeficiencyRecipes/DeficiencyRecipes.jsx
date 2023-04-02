b5974e223d3a31d867318c6699787bd870a09f6b
irenecoder <wairene60@gmail.com>
2023-04-02 18:44:27 +0300

    Case Riboflavin

--- src/pages/DeficiencyRecipes/DeficiencyRecipes.jsx
+++ src/pages/DeficiencyRecipes/DeficiencyRecipes.jsx
@@ -103,14 +103,14 @@ const DeficiencyRecipes = () => {
                           
                 }
               break;
-                case '63ffdc60c5a101405f400d7f':
-                  // Check if the object has the key whose value you want to change
-                  if (deficiency.Nutrients) {
-                    // Change the value of the key
-                    deficiency.Nutrients = 'Na_in_mg';
+            case '63ffdc60c5a101405f400d76':
+              // Check if the object has the key whose value you want to change
+              if (deficiency.Nutrients) {
+                // Change the value of the key
+                deficiency.Nutrients = 'Riboflavin_in_mcg';
                             
-                    }
-                  break;
+                }
+              break;
                   case '63ffdc60c5a101405f400d7f':
                     // Check if the object has the key whose value you want to change
                     if (deficiency.Nutrients) {
